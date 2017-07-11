95de8ec4e86156d0e110fca869692fc37046ab81
Marissa Bush <marissanbush1@verizon.net>
2017-07-11 13:54:05 -0700

    basic view controllers and classes created for screens 1-6

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EatUps
+//
+//  Created by John Abreu on 7/11/17.
+//  Copyright © 2017 John Abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
