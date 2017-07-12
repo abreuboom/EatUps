c612b1e1b934a51990da20cbadab30d023a03fcc
John Abreu <johnabreu@fb.com>
2017-07-12 14:19:43 -0700

    Updated README and removed redundant pods

--- EatUps/AppDelegate.swift
+++ EatUps/AppDelegate.swift
@@ -14,9 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    var ref: DatabaseReference!
+    let ref = Database.database().reference()
+
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
         return true
     }
 
