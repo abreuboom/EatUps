9a168936537cea56ded79c3ecc1393bec3a3e54d
John Abreu <johnabreu@fb.com>
2017-07-12 11:50:57 -0700

    Migrated Firebase to eatups email. Created swift files for User, Org, and EatUp models. Added AlamoFire & Keychain pods

---
+++ EatUps/Org.swift
@@ -0,0 +1,9 @@
+//
+//  Org.swift
+//  EatUps
+//
+//  Created by John Abreu on 7/12/17.
+//  Copyright © 2017 John Abreu. All rights reserved.
+//
+
+import Foundation
---
+++ EatUps/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  
+//
+//  Created by John Abreu on 7/12/17.
+//
+//
+
+import Foundation
