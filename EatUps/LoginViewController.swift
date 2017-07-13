1f2d48c0101552082245488a13c586df91703048
John Abreu <johnabreu@fb.com>
2017-07-13 11:15:38 -0700

    Deleted redundant objects in login view controller

--- EatUps/LoginViewController.swift
+++ EatUps/LoginViewController.swift
@@ -14,21 +14,6 @@ class LoginViewController: UIViewController, LoginButtonDelegate {
     
     @IBOutlet weak var logoImage: UIImageView!
     
-    @IBOutlet weak var usernameTextField: UITextField!
-
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-   
-    
-    @IBAction func didTapLogin(_ sender: UIButton) {
-        
-    }
-    
-    @IBAction func didTapSignup(_ sender: Any) {
-        
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
