cb931aecddb75e923b90419bacc54951dd0b0d0a
Maxine Kwan <maxinek@fb.com>
2017-07-14 16:28:47 -0700

    Added user interactions of selecting an available user

--- EatUps/UserFeedViewController.swift
+++ EatUps/UserFeedViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class UserFeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    // let User.location.availableUsers = availableUsers
+    var availableUsers: [User] = []
+    var selectedUser = User()
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var eatUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Load available users into table view
+        // availableUsers = User.location.availableUsers
         
         // Initialise collection view
         collectionView.dataSource = self
@@ -25,12 +30,27 @@ class UserFeedViewController: UIViewController, UICollectionViewDataSource, UICo
     // Configuring collection view cell views
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "userCell", for: indexPath) as! UserCell
+         cell.user = availableUsers[indexPath.item]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // return availableUsers.count - 1
         return 4
+    }
+    
+    // Changes views and stores selected user
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! UserCell
+        if(cell.isSelected) {
+            cell.backgroundColor = UIColor.red
+            selectedUser = cell.user
+            eatUpButton.isHidden = false
+        }
+        else {
+            cell.backgroundColor = UIColor.white
+            eatUpButton.isHidden = true
+        }
     }
     
     
