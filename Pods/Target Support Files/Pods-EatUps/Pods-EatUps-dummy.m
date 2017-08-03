89c5d1395289e8bd60638d3fa78a4b748b78641a
John Abreu <johnabreu@fb.com>
2017-08-02 17:33:02 -0700

    Fixed conflict in Manifest.lock

---
+++ Pods/Target Support Files/Pods-EatUps/Pods-EatUps-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EatUps : NSObject
+@end
+@implementation PodsDummy_Pods_EatUps
+@end
---
+++ Pods/Target Support Files/Pods-EatUpsUITests/Pods-EatUpsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EatUpsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EatUpsUITests
+@end
